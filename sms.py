2e19592de3d3be9938d0d549535ef62424a434a7
aliyazdani04 <98017374+aliyazdani04@users.noreply.github.com>
2022-04-12 19:11:52 +0430

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,7 +1,7 @@
 import requests
 from termcolor import colored
 
-url = ('https://app.snapp.taxi/api/api-passenger-oauth/v2/otp','https://tap33.me/api/v2/user')
+url = 'https://tap33.me/api/v2/user'
 
 phonen = input('phone number: ')
 
