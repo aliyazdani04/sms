15dc6459398383f8abfe733b312a621ac4f09c7b
aliyazdani04 <98017374+aliyazdani04@users.noreply.github.com>
2022-04-12 18:41:31 +0430

    Update sms.py

--- sms.py
+++ sms.py
@@ -2,6 +2,8 @@ import requests
 from termcolor import colored
 
 url = 'https://app.snapp.taxi/api/api-passenger-oauth/v2/otp'
+url = 'https://l3.classino.com/user/auth/otp/login'
+
 phonen = input('phone number: ')
 
 pnd = {'cellphone': f'{phonen}'}
